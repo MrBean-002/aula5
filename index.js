ea606ca0d53d95dc4479b518e20bc997de4f8f15
MrBean-002 <jvitordarosamarques@gmail.com>
2024-09-19 11:45:42 -0300

    aula5

---
+++ index.js
@@ -0,0 +1,199 @@
+
+/*console.log("ola, mundo!")
+prompt("Qual é o seu nome")
+
+ //const nome = "João"
+//let sobrenome = "Vítor"
+//const idade = 15
+let estudante = false
+
+//console.log("meu nome é:", nome , sobrenome ,"minha idade é:" + idade + " . sou estudante?", estudante)
+
+const nome = prompt ("Qual é o seu nome")
+const idade = prompt ("Qual é sua idade")
+
+console.log(typeof nome, typeof idade)*/
+
+
+
+
+
+/*let a = 10
+let b = 10
+//b = 10
+console.log(b)
+
+b = 5
+console.log(a, b)
+//a = 10 b =5
+
+let a = 10
+let b = 20
+c = a //vale 10
+b = c // vale 10
+a = b //vale 10
+
+console.log(a, b, c)
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+// vai aparecer a divisao disso ${t/p*/
+
+/*var  nome = prompt ("Qual é o seu nome")
+var  idade = prompt ("Qual é a sua idade")
+
+//console.log(typeof nome, typeof idade) //apareceu string string tipo texto porque eu pedi
+
+//console.log("Qual seu nome", nome, "Qual é a sua idade", idade)
+
+//console.log("ola nome", nome, "qual é sua idade", idade */
+
+/*const primeiroValor = 4
+const segundoValor = 3
+
+const resultado = primeiroValor + segundoValor 
+
+console.log(resultado)*/
+
+/*const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor - segundoValor
+
+console.log(resultado)*/
+
+/*const primeiroValor = 10
+const segundoValor = 25
+
+const resultado = primeiroValor* segundoValor
+
+console.log(resultado)*/
+
+/*const primeiroValor = 345
+const segundovalor = 10
+
+const  resultado = primeiroValor/ segundovalor
+
+console.log(resultado)*/
+
+/*const A = true
+const B = false
+const C = true
+
+A||B
+B||C
+A||C
+A||B||C*/
+
+
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2 && bool3 
+
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)*///Vai aparecer true,false e false
+
+/*let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const resultado = primeiroNumero + segundoNumero
+
+console.log(resultado)// vai aparecer os numeros que voce colocar, juntados*/
+
+/*let idadeUsuario = Number(prompt("Qual a sua idade?"))
+let idadeAmigo = Number(prompt("Qual a idade do seu amigo(A)"))
+
+console.log("Sua idade é maior do que a do seu melhor amigo?, idadeUsuario >=idadeAmigo")
+console.log("diferença de idade:", Number(idadeUsuario-idadeAmigo))
+
+let numPar = Number(prompt("Insira um numero par"))
+const resultado = Number(numPar/2)
+
+console.log(resultado)//não notei padrão 
+
+const restoDaDivisão = (numPar%2)
+console.log(restoDaDivisão)*/
+
+
+/*let idade = Number(prompt("Qual a sua idade ?"))
+
+const idadeMeses = idade*12
+resultado=idade*12 
+console.log(resultado)
+
+const idadeAnos = idade*365
+resultado1=idade*365
+console.log(resultado1)
+
+const idadeHoras = resultado1*24
+resultado=resultado1*24
+console.log(resultado)*/
+
+
+/*let numero1 = Number(prompt("Digite um numero"))
+let numero2 = Number(prompt("Digite outro numero"))
+
+console.log(numero1 > numero2)*/
+
+/*let nome = prompt("Qual seu nome")
+let corfav = prompt("Qual sua cor favorita")
+
+const frase = (" A cor favorita de " + nome + "é" + corfav )
+
+console.log(frase)*/
+
+/*let frase = prompt("escreva uma frase")
+const fraseMaiuscula = frase.toUpperCase(frase)
+console.log(fraseMaiuscula)
+
+const novaFrase =frase.replaceAll("o", "i" )
+console.log(novaFrase)
+
+console.log(frase.length)*/
+
+/*let frase = prompt("escreva de 0 a 4, raças de cachoro ")
+const raçaDeCacho = ["Pastor-alemão", "Buldoque", "poodle", "Chihuahua", "Beagle"]
+const raça = raçaDeCacho[4]
+console.log(raça)*/
+
+const numeros = [1,2,3,4,5,6]
+console.log(numeros.length)
+numeros.push(7)
+console.log(numeros)
+numeros.splice(3,2)
+console.log(numeros)
+console.log(numeros.length)
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
